33dae3d7b31b29604d56c8eb4d0795c18b7861c4
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-27 20:33:46 +0700

    a

--- buoi1.js
+++ buoi1.js
@@ -1,1 +1,16 @@
-"Hello"
+// "Hello"
+// ctrl + / : Comment code không sử dụng
+// Mở môi trường thực thi  
+// Macos : control + `
+// Window : ctrl + ` 
+// ngắt thực thi : ctrl + c
+// var a = "hello"
+// var b = "xin chao"
+
+// console.log(b)
+//1 : Khai báo biến : var let const
+
+const a
+console.log(a)
+
+
