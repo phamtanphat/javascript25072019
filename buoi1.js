146dc5c33b261ac0663fdd8a14cc9917c407176b
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-27 20:50:39 +0700

    toantu

--- buoi1.js
+++ buoi1.js
@@ -10,7 +10,21 @@
 // console.log(b)
 //1 : Khai báo biến : var let const
 
-const a
-console.log(a)
+// const a
+// console.log(a)
+// 2 : Toán tử (+ , - , * , /)
+// var a = 5 + 6 //(Biểu thức)
+// var b = a++ //(Biểu thức)
+// var c = (a > b) ? a : b //(Toán tử 3 ngôi)
+var a = 5
+var b = 6
+var c = ++a + b++ - a++ + b-- + a++ + b--
+//6 + b++ - a++ + b-- + a++ + b-- : a = 6 
+// 6 + 6 - a++ + b-- + a++ + b-- : a = 6 , b = 7
+// 6 + 6 - 6 + b-- + a++ + b-- : a = 7 , b = 7
+//6 + 6 - 6 + 7 + a++ + b-- : a = 7 , b = 6
+//6 + 6 - 6 + 7 + 7 + b-- : a = 8 , b = 6
+//6 + 6 - 6 + 7 + 7 + 6 = 26
+console.log(c)
 
 
